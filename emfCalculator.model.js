3e89e8335705681013c384ac88fc8acc725db19c
Zigotote <oce88b@gmail.com>
2021-07-01 19:48:49 +0200

    Upd: correct emf frequencies

--- emfCalculator.model.js
+++ emfCalculator.model.js
@@ -1,38 +1,38 @@
 class EmfCalculator {
-	EMF_FREQUENCIES(ind) {
-		const EMF_FREQUENCIES = [8, 16, 32, 4];
-		return EMF_FREQUENCIES[ind];
-	}
-	constructor(emfMax) {
-		this.emfMax = emfMax;
-		this.frequency = 1;
-		this.emfValue = 0;
-		this.isHunting = false;
-	}
+  EMF_FREQUENCIES(ind) {
+    const EMF_FREQUENCIES = [32, 16, 8, 4];
+    return EMF_FREQUENCIES[ind];
+  }
+  constructor(emfMax) {
+    this.emfMax = emfMax;
+    this.frequency = 1;
+    this.emfValue = 0;
+    this.isHunting = false;
+  }
 
-	startHunting() {
-		if (!this.isHunting) {
-			this.oldEmfFrequency = this.frequency;
-			this.frequency = 3;
-			this.isHunting = true;
-		}
-	}
+  startHunting() {
+    if (!this.isHunting) {
+      this.oldEmfFrequency = this.frequency;
+      this.frequency = 3;
+      this.isHunting = true;
+    }
+  }
 
-	recupOldFrequency() {
-		this.frequency = this.oldEmfFrequency;
-	}
+  recupOldFrequency() {
+    this.frequency = this.oldEmfFrequency;
+  }
 
-	getEmfValue(isGhostPresent) {
-		if (this.isHunting || isGhostPresent) {
-			this.emfValue = Math.round(Math.random() * this.emfMax);
-		} else {
-			this.emfValue = 0;
-		}
-		return this.emfValue;
-	}
+  getEmfValue(isGhostPresent) {
+    if (this.isHunting || isGhostPresent) {
+      this.emfValue = Math.round(Math.random() * this.emfMax);
+    } else {
+      this.emfValue = 0;
+    }
+    return this.emfValue;
+  }
 
-	getEmfTimeFrequency() {
-		return this.EMF_FREQUENCIES(this.frequency) * 1000;
-	}
+  getEmfTimeFrequency() {
+    return this.EMF_FREQUENCIES(this.frequency) * 1000;
+  }
 }
 module.exports = EmfCalculator;
